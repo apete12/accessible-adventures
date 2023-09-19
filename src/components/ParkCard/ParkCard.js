9adbb31a97c4f3a91891a02ae3346a81d2179880
Alex Peterson <aspeterson.126@gmail.com>
2023-09-19 14:49:31 -0500

    Add grid styling to AllParks component

--- src/components/ParkCard/ParkCard.js
+++ src/components/ParkCard/ParkCard.js
@@ -2,7 +2,7 @@ import './ParkCard.css'
 
 const ParkCard = (props) => {
 return (
-    <div className='park-card'>
+    <div className='park-card' key={props.id}>
       <h2>{props.name}</h2>
       <h3>{props.state}</h3>
     </div>
